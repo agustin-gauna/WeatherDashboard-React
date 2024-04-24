0a8c8522f86e9a76e3133b9b73ad2647caba663e
Agustin <agustin.gauna@outlook.com.ar>
2024-04-24 15:03:26 -0300

    api change

--- src/weatherService/accuWeather.js
+++ src/weatherService/accuWeather.js
@@ -2,6 +2,7 @@
 
 const API_KEY = "sQoF7TGo6pGG1mIXyLDhHzXyXAniPDjt";
 const API_KEY2 = "bAJhVBBEveSR1Tcxweif1EkbhZPJdFtn"; 
+const API_KEY3 = "0412g52Y4mkGBlG2eEpHzHlNWj6aLdZq"
 
 
 const API_KEY_OPENWEATHER = "6419b61865ae340750eb256dda6f170b"; // api key de openweather
@@ -13,7 +14,7 @@ const getCity = async (city) => {
     const BASE_URL = "http://dataservice.accuweather.com/locations/v1/cities/autocomplete"
     // const QUERY = `?apikey=${API_KEY}&q=${city}`
     const url = new URL(BASE_URL)
-    url.searchParams.append('apikey', API_KEY)
+    url.searchParams.append('apikey', API_KEY3)
     url.searchParams.append('q',city)
 
     const response = await fetch(url)
@@ -28,7 +29,7 @@ const getWeather = async(locationKey) => {
     const BASE_URL_5DAY = "http://dataservice.accuweather.com/forecasts/v1/daily/5day/"
     const BASE_URL_12HOUR = "http://dataservice.accuweather.com/forecasts/v1/hourly/12hour/"
 
-    const QUERY = `?apikey=${API_KEY}&metric=true`
+    const QUERY = `?apikey=${API_KEY3}&metric=true`
 
     const response5Day = await fetch(BASE_URL_5DAY + locationKey + QUERY)
     const data5Day = await response5Day.json()
